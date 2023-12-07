27e16f1ea4c59d8a6495ff8234e8fa48e5be7d99
DariaYudina004 <yudina.daria.antonovna@gmail.com>
2023-12-08 00:15:49 +0300

    Old Version of test panel

--- LangARise/Assets/Scripts/QuestionManager.cs
+++ LangARise/Assets/Scripts/QuestionManager.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class QuestionManager : MonoBehaviour
 {
     [SerializeField] private QuestionsList qList; // »з списка присваиваютс€ значени€ одному вопросу 
-    public QuestionsList QList => qList; //
-    [SerializeField] private QuestionsBase oneVersion; // ќдин Ёкземпл€р
-    //
+    public QuestionsList QList => qList; 
+    private int oneVersion; // ќдин Ёкземпл€р
+
     [SerializeField] private TextMeshProUGUI questionText; // —юда уходит сам вопрос
     public TextMeshProUGUI QuestText { get { return questionText; } set { questionText = value; } }
 
@@ -15,15 +15,29 @@ public class QuestionManager : MonoBehaviour
 
 
 
-    public void OnClickPlay()
+    public void GenerateQuestionFromList()
     {
-        oneVersion = qList.questionsInList[Random.Range(0, QList.questionsInList.Count)];
-        QuestText.text = oneVersion.QuestionsOnBase;
-        for (int i = 0; i < oneVersion.answersOnBase.Count; i++)
+        if(QList.questionsInList.Count > 0)
         {
-            answersOfOneQuestion[i].text = oneVersion.answersOnBase[i];
+            oneVersion = Random.Range(0, QList.questionsInList.Count);
+            //oneVersion = qList.questionsInList[Random.Range(0, QList.questionsInList.Count)];
+            QuestText.text = QList.questionsInList[oneVersion].QuestionsOnBase;
+            for (int i = 0; i < QList.questionsInList[oneVersion].answersOnBase.Count; i++)
+            {
+                int rand = Random.Range(0, QList.questionsInList[oneVersion].answersOnBase.Count);
+                answersOfOneQuestion[i].text = QList.questionsInList[oneVersion].answersOnBase[rand];
+                QList.questionsInList[oneVersion].answersOnBase.RemoveAt(rand);
+                Debug.Log(i);
+            }
         }
+        
+    }
 
-
+    public void DeleteQuestionFromListAfterClickNextButton(int index)
+    {
+        if (answersOfOneQuestion[index].text.ToString() == QList.questionsInList[oneVersion].answersOnBase[0]) Debug.Log("OK");
+        else Debug.Log("Not OK");
+        QList.questionsInList.RemoveAt(oneVersion);
+        GenerateQuestionFromList();
     }
 }
