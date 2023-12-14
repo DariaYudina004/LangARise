6737a5ea3a23e174fbf92aed209e63c9c22cc8c7
DariaYudina004 <yudina.daria.antonovna@gmail.com>
2023-12-14 14:01:15 +0300

    yes

--- LangARise/Assets/Scripts/QuestionManager.cs
+++ LangARise/Assets/Scripts/QuestionManager.cs
@@ -26,13 +26,14 @@ public class QuestionManager : MonoBehaviour
         {
             oneVersion = Random.Range(0, oneFromList.Count);
             //oneVersion = qList.questionsInList[Random.Range(0, QList.questionsInList.Count)];
-            currentBase = oneFromList[oneVersion] as QuestionsBase;
+             currentBase = oneFromList[oneVersion] as QuestionsBase;
             questionText.text = currentBase.QuestionsOnBase;
-            for (int i = 0; i < currentBase.answersOnBase.Count; i++)
+            List<string> answers = new List<string>(currentBase.answersOnBase);
+            for (int i = 0; i < currentBase.answersOnBase.Length; i++)
             {
-                int rand = Random.Range(0, currentBase.answersOnBase.Count);
-                answersOfOneQuestion[i].text = currentBase.answersOnBase[rand];
-                //currentBase.answersOnBase.RemoveAt(rand);
+                int rand = Random.Range(0, answers.Count);
+                answersOfOneQuestion[i].text = answers[rand];
+                answers.RemoveAt(rand);
                 Debug.Log(i);
             }
         }
@@ -45,14 +46,15 @@ public class QuestionManager : MonoBehaviour
 
     public void DeleteQuestionFromListAfterClickNextButton(int index)
     {
-        if (answersOfOneQuestion[index].text.ToString() == currentBase.answersOnBase[0])
-        {
-            questionText.text = "Правильный ответ";
-        }
-        else
-        {
-            questionText.text = "Неправильный ответ :(";
-        }
+        Debug.Log("answersOfOneQuestion[index]" , answersOfOneQuestion[index]);
+        //if (answersOfOneQuestion[index].text.ToString() == currentBase.answersOnBase[0])
+        //{
+        //    questionText.text = "Правильный ответ";
+        //}
+        //else
+        //{
+        //    questionText.text = "Неправильный ответ :(";
+        //}
         oneFromList.RemoveAt(oneVersion);
         GenerateQuestionFromList();
     }
